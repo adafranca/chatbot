444f7dd2632ff8cbf108830e7f981833f209e26f
adacf <adacristinafranca@gmail.com>
2018-11-28 18:52:28 -0200

    data processing almost done. It's necessary to refactor the code

--- chatbot.py
+++ chatbot.py
@@ -18,14 +18,12 @@ for line in lines:
         dic[_line[0]] = _line[4]
 
 
-print(dic)
-
 #creating a list of all conversations
 conversations_id = []
 # There are a empty conversation in the end, because of this -1
 for conversation in conversations[:-1]:
     _conversation = conversation.split(" +++$+++ ")[-1][1:-1].replace("'", "").replace(" ","")
-    conversations_id.append(conversation.split(','))
+    conversations_id.append(_conversation.split(','))
 
 #separate between questions and answers ( x and y )
 questions = []
@@ -52,14 +50,76 @@ def clean_text(text):
     text = re.sub(r"\'d", " would", text)
     text = re.sub(r"won't", "will not", text)
     text = re.sub(r"can't", "cannot", text)
-    text = re.sub(r"[-()\"#/@;:<>{}+=-|.?,]", "", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
     return text
+
+
+word_counts = {}
 
 clean_questions = []
 for question in questions:
-    clean_questions.append(clean_text(question))
-
+    _question = clean_text(question)
+    clean_questions.append(_question)
+    for word in _question.split(" "):
+        if word not in word_counts:
+            word_counts[word] = 1
+        else:
+            word_counts[word] += 1
 
 clean_answers = []
 for answer in answers:
-    clean_answers.append(clean_text(answer))
+    _answer = clean_text(answer)
+    clean_answers.append(_answer)
+    for word in _answer.split(" "):
+        if word not in word_counts:
+            word_counts[word] = 1
+        else:
+            word_counts[word] += 1
+
+
+threshold = 20
+word_number = 0
+questionswords2int = {}
+answersword2int = {}
+for word, count in word_counts.items():
+    if count >= threshold:
+        questionswords2int[word] = word_number
+        word_number+=1
+
+
+answersword2int = questionswords2int
+
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int)+1
+    answersword2int[token] = len(answersword2int) + 1
+
+answersint2word = { w_i: w for w, w_i in answersword2int.items()}
+
+questionsint2word = { w_i: w for w, w_i in answersword2int.items()}
+
+for i in range(len(clean_answers)):
+    clean_answers[i] += " <EOS>"
+
+code_questions = []
+for question in clean_questions:
+    _question = []
+    for word in question.split():
+        if word not in questionswords2int:
+            _question.append(questionswords2int["<OUT>"])
+        else:
+            _question.append(questionswords2int[word])
+    code_questions.append(_question)
+
+code_answers = []
+for answers in clean_answers:
+    _answer = []
+    for word in answers.split():
+        if word not in answersword2int:
+            _answer.append(answersword2int["<OUT>"])
+        else:
+            _answer.append(answersword2int[word])
+    code_answers.append(_answer)
+
+
